055a3f05cdd52de8c49b91433a0e80115f3e1e99
薛翔 <xuexiangjys@163.com>
2018-04-07 23:10:08 +0800

    修改演示代码

--- app/src/main/java/com/xuexiang/xmarkdemo/MainActivity.java
+++ app/src/main/java/com/xuexiang/xmarkdemo/MainActivity.java
@@ -1,9 +1,10 @@
 package com.xuexiang.xmarkdemo;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import com.xuexiang.xmark.XMark;
 import com.xuexiang.xmark.annotation.MarkLog;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +15,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         printArgs("The", "Quick", "Brown", "Fox");
+
+        Log.i("Fibonacci", "fibonacci's 4th number is " + fibonacci(4));
+
+        Greeter greeter = new Greeter("Jake");
+        Log.d("Greeting", greeter.sayHello());
+
+        Charmer charmer = new Charmer("Jake");
+        Log.d("Charming", charmer.askHowAreYou());
+
+        startSleepyThread();
     }
 
 
     @MarkLog
     private void printArgs(String... args) {
         for (String arg : args) {
-            XMark.log("Args", arg);
+            Log.i("Args", arg);
+        }
+    }
+
+
+    @MarkLog
+    private int fibonacci(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Number must be greater than zero.");
+        }
+        if (number == 1 || number == 2) {
+            return 1;
+        }
+        // NOTE: Don't ever do this. Use the iterative approach!
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+
+    private void startSleepyThread() {
+        new Thread(new Runnable() {
+            private static final long SOME_POINTLESS_AMOUNT_OF_TIME = 50;
+
+            @Override public void run() {
+                sleepyMethod(SOME_POINTLESS_AMOUNT_OF_TIME);
+            }
+
+            @MarkLog
+            private void sleepyMethod(long milliseconds) {
+                SystemClock.sleep(milliseconds);
+            }
+        }, "I'm a lazy thr.. bah! whatever!").start();
+    }
+
+    @MarkLog
+    static class Greeter {
+        private final String name;
+
+        Greeter(String name) {
+            this.name = name;
+        }
+
+        private String sayHello() {
+            return "Hello, " + name;
+        }
+    }
+
+    @MarkLog
+    static class Charmer {
+        private final String name;
+
+        private Charmer(String name) {
+            this.name = name;
+        }
+
+        public String askHowAreYou() {
+            return "How are you " + name + "?";
         }
     }
 }
